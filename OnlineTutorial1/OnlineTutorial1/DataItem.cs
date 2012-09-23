be06dc6307293cd2b008615ae3f3a70c53b8573a
Brett Yukich <brettyukich@gmail.com>
2012-09-24 09:41:28 +1200

    Added the completed first online tutorial

---
+++ OnlineTutorial1/OnlineTutorial1/DataItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OnlineTutorial1
+{
+    public class DataItem
+    {
+        private string name;
+        private bool canDrop;
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public bool CanDrop
+        {
+            get { return canDrop; }
+        }
+
+        public DataItem(string name, bool canDrop)
+        {
+            this.name = name;
+            this.canDrop = canDrop;
+        }
+    }
+}
