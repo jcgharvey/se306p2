963246ccc91de69760dab88c1dde1a23e97d258c
Michael Little <bio595@gmail.com>
2012-09-26 16:33:20 +1200

    adding project that demonstrates screen switching
    Uses UserControls and the SurfaceWindow's Content property to switch out
    various screens.

---
+++ ECE_Showcase/ECE_Showcase/Screens/HomeScreen.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ECE_Showcase.Screens
+{
+    /// <summary>
+    /// Interaction logic for HomeScreen.xaml
+    /// </summary>
+    public partial class HomeScreen : UserControl
+    {
+        public HomeScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void InfoButton_Click(object sender, RoutedEventArgs e)
+        {
+            ((SurfaceWindow1)(this.Parent)).Content = new InfoScreen();
+        }
+
+        private void HODButton_Click(object sender, RoutedEventArgs e)
+        {
+            ((SurfaceWindow1)(this.Parent)).Content = new HODWelcome();
+        }
+    }
+}
