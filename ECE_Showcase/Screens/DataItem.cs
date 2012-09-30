687294f5745ee241618e014106eefc05a2c1e9d7
brettyukich <brettyukich@gmail.com>
2012-10-01 09:17:25 +1300

    created missing file

---
+++ ECE_Showcase/Screens/DataItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ECE_Showcase.Screens
+{
+    public class DataItem
+    {
+        private string name;
+        public string filePath;
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public string FilePath
+        {
+            get { return filePath; }
+        }
+
+        public DataItem(string name, string filePath)
+        {
+            this.name = name;
+            this.filePath = filePath;
+        }
+    }
+}
