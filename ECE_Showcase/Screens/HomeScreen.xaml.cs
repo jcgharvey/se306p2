29f5e69cdfa73132f849ab0ba27aa6f540b41a75
Dave Carpenter <dave@ungarst.com>
2012-10-03 14:48:27 +1300

    Implemented zoom gesture

--- ECE_Showcase/Screens/HomeScreen.xaml.cs
+++ ECE_Showcase/Screens/HomeScreen.xaml.cs
@@ -24,6 +24,10 @@ namespace ECE_Showcase.Screens
         private InfoScreen infoScreen;
         private HODWelcomeScreen hodWelcomeScreen;
         private Courses coursesScreen;
+        private TouchPoint touch1;
+        private TouchPoint touch2;
+        private double initialDist;
+        private bool triggered;
 
         public HomeScreen(SurfaceWindow1 parentWindow) : base(parentWindow)
         {
@@ -31,6 +35,10 @@ namespace ECE_Showcase.Screens
 
             infoScreen = null;
             hodWelcomeScreen = null;
+            touch1 = null;
+            touch2 = null;
+            initialDist = Double.MaxValue;
+            triggered = false;
         }
 
         private void InfoButton_Click(object sender, RoutedEventArgs e)
@@ -63,33 +71,70 @@ namespace ECE_Showcase.Screens
         }
 
 
-       
-
         private void RNDButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void HODButton_TouchUp(object sender, TouchEventArgs e)
+        private void Button_TouchUp(object sender, TouchEventArgs e)
         {
-            if (hodWelcomeScreen == null)
+            if (touch1 == null ^ touch2 == null)
             {
-                hodWelcomeScreen = new HODWelcomeScreen(ParentWindow);
+                if (hodWelcomeScreen == null)
+                {
+                    hodWelcomeScreen = new HODWelcomeScreen(ParentWindow);
+                }
+                ParentWindow.pushScreen(hodWelcomeScreen);
             }
-            ParentWindow.pushScreen(hodWelcomeScreen);
+
+            touch1 = null;
+            touch2 = null;
+
         }
 
-        private void HODButton_PreviewTouchMove(object sender, TouchEventArgs e)
+        private void Button_PreviewTouchMove(object sender, TouchEventArgs e)
         {
-            
+            if (touch1 != null && touch2 != null)
+            {
+                //Debug.WriteLine((sender as SurfaceButton).Name);
+                TouchPoint newTouch = e.GetTouchPoint(sender as SurfaceButton);
+                
+                if (touchDist(newTouch, touch1) < touchDist(newTouch, touch2))
+                {
+                    touch1 = newTouch;
+                }
+                else
+                {
+                    touch2 = newTouch;
+                }
+
+                if (touchDist(touch1, touch2) - initialDist > 75 && !triggered)
+                {
+                    triggered = false;
+                    MessageBox.Show((sender as SurfaceButton).Name);
+                }
+
+            }
+           
         }
 
-        private void HODButton_PreviewTouchDown(object sender, TouchEventArgs e)
+        private double touchDist(TouchPoint t1, TouchPoint t2)
         {
-           TouchPoint touchPoint = e.GetTouchPoint(sender as SurfaceButton);
-            Debug.WriteLine(touchPoint.Position.X + " " + touchPoint.Position.Y);
+            return Math.Sqrt(Math.Pow((t1.Position.X - t2.Position.X), 2) + Math.Pow((int)(t1.Position.Y - t2.Position.Y), 2));
+        }
 
-
+        private void Button_PreviewTouchDown(object sender, TouchEventArgs e)
+        {
+            if (touch1 == null)
+            {
+                touch1 = e.GetTouchPoint(sender as SurfaceButton);
+            }
+            else
+            {
+                touch2 = e.GetTouchPoint(sender as SurfaceButton);
+               
+                initialDist = touchDist(touch1, touch2);
+            }
         }
     }
 }
