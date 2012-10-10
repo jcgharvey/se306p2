a871828cb58c8c2265ad1451fda1634fc1712655
brettyukich <brettyukich@gmail.com>
2012-10-10 15:22:04 +1300

    implemented accordionwith basic template

--- ECE_Showcase/Screens/Courses.xaml.cs
+++ ECE_Showcase/Screens/Courses.xaml.cs
@@ -65,7 +65,6 @@ namespace ECE_Showcase.Screens
             SourceItems.Add(new DataItem("Software Engineering", "Resources/docs/specialisations/se.xaml", "../Resources/img/software.png"));
             SourceItems.Add(new DataItem("Electrical and Electronic Engineering", "Resources/docs/specialisations/eee.xaml", "../Resources/img/electrical.png"));
             SourceItems.Add(new DataItem("Computer Systems Engineering", "Resources/docs/specialisations/cse.xaml", "../Resources/img/compsys.png"));
-
             infoViewer.Document = (FlowDocument)XamlReader.Load(File.OpenRead("Resources/docs/specialisations/se.xaml"));
             
         }
