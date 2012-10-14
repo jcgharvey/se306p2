346d13b577d94c1dc9f1ad1a74d0eea4b01d1344
brettyukich <brettyukich@gmail.com>
2012-10-15 08:56:26 +1300

    added Image back into dataItem

--- ECE_Showcase/Screens/DataItem.cs
+++ ECE_Showcase/Screens/DataItem.cs
@@ -10,11 +10,19 @@ namespace ECE_Showcase.Screens
     {
         public string Name { get; private set; }
         public UserControl ItemControl { get; private set;}
+        public Image ItemImage { get; private set; }
 
         public DataItem(string name, UserControl control)
         {
             Name = name;
             ItemControl = control;
+        }
+
+        public DataItem(string name, UserControl control, Image image)
+        {
+            Name = name;
+            ItemControl = control;
+            ItemImage = image;
         }
     }
 }
