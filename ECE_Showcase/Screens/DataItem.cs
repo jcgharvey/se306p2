860308cf5aeefb3d6a39e2fb82ba863a2fe93610
brettyukich <brettyukich@gmail.com>
2012-10-14 23:10:31 +1300

    refactored DataItem for UserControls

--- ECE_Showcase/Screens/DataItem.cs
+++ ECE_Showcase/Screens/DataItem.cs
@@ -2,35 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Controls;
 
 namespace ECE_Showcase.Screens
 {
     public class DataItem
     {
-        private string name;
-        public string filePath;
-        public string imagePath;
+        private string Name { public get; set; }
+        private UserControl ItemControl { public get; set;}
 
-        public string Name
+        public DataItem(string name, UserControl control)
         {
-            get { return name; }
-        }
-
-        public string FilePath
-        {
-            get { return filePath; }
-        }
-
-        public string ImagePath
-        {
-            get { return imagePath; }
-        }
-
-        public DataItem(string name, string filePath, string imagePath)
-        {
-            this.name = name;
-            this.filePath = filePath;
-            this.imagePath = imagePath;
+            Name = name;
+            ItemControl = control;
         }
     }
 }
