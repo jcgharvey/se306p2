3dda14e4064248ba648275a792dc694dd35bf3fe
brettyukich <brettyukich@gmail.com>
2012-10-14 16:10:29 +1300

    created courseItem class with getters and setters

---
+++ ECE_Showcase/Screens/CoursesItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ECE_Showcase.Screens
+{
+    public class CourseItem
+    {
+        private string Name { get; private set; }
+        private string Number { get; private set; }
+        private string Info { get; private set; }
+        private string Points { get; private set; }
+        private string Prereq { get; private set; }
+
+        public CourseItem(string name, string number, string info, string points, string prereq)
+        {
+            Name = name;
+            Number = number;
+            Info = info;
+            Points = points;
+            Prereq = prereq;
+        }
+    }
+}
