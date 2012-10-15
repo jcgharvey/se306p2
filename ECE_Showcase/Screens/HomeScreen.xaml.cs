47143bc75754e29a267128c63d9aa1cf2e80b87e
Andrew Luey <alue004@aucklanduni.ac.nz>
2012-10-15 20:57:12 +1300

    merged finishZoom into Master

--- ECE_Showcase/Screens/HomeScreen.xaml.cs
+++ ECE_Showcase/Screens/HomeScreen.xaml.cs
@@ -22,14 +22,22 @@ namespace ECE_Showcase.Screens
     /// </summary>
     public partial class HomeScreen : Screen
     {
+
+        
+
         private TouchPoint touch1;
         private TouchPoint touch2;
         private double initialDist;
         private bool triggered;
 
+       
+        private SurfaceButton newImage;
+
         public HomeScreen(SurfaceWindow1 parentWindow) : base(parentWindow)
         {
             InitializeComponent();
+
+     
 
             touch1 = null;
             touch2 = null;
@@ -53,7 +61,7 @@ namespace ECE_Showcase.Screens
                            ((FirstLevelScreen)screenToPush).setRight(new Controls.FlowDocControl("Resources/docs/info.xaml"));
 
                         break;
-                    case "HODButton":
+                    case "HodButton":
                             screenToPush = new FirstLevelScreen(ParentWindow, "department welcome");
                             ((FirstLevelScreen)screenToPush).setLeft(new Controls.ImageControl("/ECE_Showcase;component/Resources/img/salcic.jpg"));
                             ((FirstLevelScreen)screenToPush).setRight(new Controls.FlowDocControl("Resources/docs/hod_welcome.xaml"));
@@ -108,16 +116,121 @@ namespace ECE_Showcase.Screens
 
                 if (touchDist(touch1, touch2) - initialDist > 75 && !triggered)
                 {
-                    triggered = false;
-                    
+                    triggered = true;
 
+                    Debug.Print((sender as SurfaceButton).Name);
                    //Animate the grid control
                    Storyboard sb;
-                   sb = this.FindResource("gridin") as Storyboard;
-                   sb.Begin(this);
-                   (sender as SurfaceButton).Content = "SUP";
+                   if((sender as SurfaceButton).Name.Equals("InfoButton"))
+                   {
+                       sb = this.FindResource("gridin") as Storyboard;
+                       sb.Begin(this);
+                       InfoButton.Height = 390;
+                     //  (sender as SurfaceButton).Content = "";
+                       newImage = (sender as SurfaceButton);
+
+                   }
+                   else if ((sender as SurfaceButton).Name.Equals("HodButton"))
+                   {
+                       sb = this.FindResource("gridin1") as Storyboard;
+                       sb.Begin(this);
+                       HodButton.Height = 390;
+
+                       newImage = (sender as SurfaceButton);
+                   }
+                   else if ((sender as SurfaceButton).Name.Equals("ProgrammesButton"))
+                   {
+                       sb = this.FindResource("gridin2") as Storyboard;
+                       sb.Begin(this);
+                       ProgrammesButton.Height = 390;
+
+                       newImage = (sender as SurfaceButton);
+                   }
+
+                   else if ((sender as SurfaceButton).Name.Equals("ContactButton"))
+                   {
+                       sb = this.FindResource("gridin3") as Storyboard;
+                       sb.Begin(this);
+                       ContactButton.Height = 390;
+
+                       newImage = (sender as SurfaceButton);
+                   }
                    
-                   (sender as SurfaceButton).Height = 370;
+                }
+                else if (touchDist(touch1, touch2) - initialDist < 75 && triggered)
+                {
+                    triggered = false;
+
+                    Storyboard sb;
+                    Image myImage3 = new Image();
+                    BitmapImage bi3 = new BitmapImage();
+
+                     if((sender as SurfaceButton).Name.Equals("InfoButton"))
+                   {
+                         sb = this.FindResource("gridout") as Storyboard;
+                         sb.Begin(this);
+                         InfoButton.Height = 195;
+                         
+                         bi3.BeginInit();
+                         bi3.UriSource = new Uri("/ECE_Showcase;component/Resources/img/info_btn.png", UriKind.Relative);
+                         bi3.EndInit();
+                         myImage3.Stretch = Stretch.Uniform;
+                         myImage3.Source = bi3;
+                         myImage3.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+
+                       
+                   }
+                   else if ((sender as SurfaceButton).Name.Equals("HodButton"))
+                   {
+                         sb = this.FindResource("gridout1") as Storyboard;
+                         sb.Begin(this);
+                         HodButton.Height = 195;
+
+                         bi3.BeginInit();
+                         bi3.UriSource = new Uri("/ECE_Showcase;component/Resources/img/HOD_btn.png", UriKind.Relative);
+                         bi3.EndInit();
+                         myImage3.Stretch = Stretch.UniformToFill;
+                         myImage3.Source = bi3;
+
+                         myImage3.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+                       
+                   }
+                     else if ((sender as SurfaceButton).Name.Equals("ProgrammesButton"))
+                     {
+                         sb = this.FindResource("gridout2") as Storyboard;
+                         sb.Begin(this);
+                         ProgrammesButton.Height = 195;
+
+                         bi3.BeginInit();
+                         bi3.UriSource = new Uri("/ECE_Showcase;component/Resources/img/Programmes_btn.png", UriKind.Relative);
+                         bi3.EndInit();
+
+                         myImage3.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+
+                     }
+
+                     else if ((sender as SurfaceButton).Name.Equals("ContactButton"))
+                     {
+                         sb = this.FindResource("gridout3") as Storyboard;
+                         sb.Begin(this);
+                         ContactButton.Height = 195;
+
+                         bi3.BeginInit();
+                         bi3.UriSource = new Uri("/ECE_Showcase;component/Resources/img/Contact_us_btn.png", UriKind.Relative);
+                         bi3.EndInit();
+
+                         myImage3.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+
+                     }
+                     myImage3.Stretch = Stretch.UniformToFill;
+
+                     myImage3.Source = bi3;
+                     Thickness thick = new Thickness(1);
+                     myImage3.Margin = thick;
+                     (sender as SurfaceButton).Margin = thick;
+
+                     (sender as SurfaceButton).Content = myImage3;
+
                 }
 
             }
@@ -142,6 +255,54 @@ namespace ECE_Showcase.Screens
                
                 initialDist = touchDist(touch1, touch2);
             }
+        }
+
+        private void Storyboard_Completed(object sender, EventArgs e)
+        {
+            Storyboard sb;
+
+            newImage.Content = "there are going to be lots of words here";
+            
+            if (newImage.Name.Equals("InfoButton"))
+            {
+                sb = this.FindResource("fadein") as Storyboard;
+                sb.Begin(this);
+
+                Image myImage3 = new Image();
+                BitmapImage bi3 = new BitmapImage();
+
+                bi3.BeginInit();
+                bi3.UriSource = new Uri("/ECE_Showcase;component/Resources/img/forthelolz.png", UriKind.Relative);
+                bi3.EndInit();
+                myImage3.Stretch = Stretch.UniformToFill;
+                myImage3.Source = bi3;
+
+                myImage3.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+
+                newImage.Content = myImage3;
+            }
+            else if (newImage.Name.Equals("HodButton"))
+            {
+                sb = this.FindResource("fadein1") as Storyboard;
+                sb.Begin(this);
+            }
+            else if (newImage.Name.Equals("ProgrammesButton"))
+            {
+                sb = this.FindResource("fadein2") as Storyboard;
+                sb.Begin(this);
+            }
+            else if (newImage.Name.Equals("ContactButton"))
+            {
+                sb = this.FindResource("fadein3") as Storyboard;
+                sb.Begin(this);
+            }
+
+        }
+
+        private void Storyboard_Completed_1(object sender, EventArgs e)
+        {
+
+
         }
 
     }
